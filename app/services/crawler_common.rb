95691fbd8981cf56a60c80e1b0a25ca705823be6
mattwd7 <mattwd7@gmail.com>
2017-05-31 22:07:28 -0400

    email regex update

--- app/services/crawler_common.rb
+++ app/services/crawler_common.rb
@@ -13,6 +13,7 @@ module CrawlerCommon
 	end
 
 	def parse_emails(text)
-		text.scan /[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/
+		text.scan(/[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/)
+			.reject { |emails| emails.match /\.(jpe?g|png|gif|bmp)$/i }
 	end
 end
